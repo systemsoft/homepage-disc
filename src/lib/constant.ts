8a2eba4b54d8e45f7455e6b37cbee6f6c9c7545c
netop://ウィビ <paul@webb.page>
2026-06-22 09:41:17 -0700

    updates version

--- src/lib/constant.ts
+++ src/lib/constant.ts
@@ -2,4 +2,4 @@
 /*** EXPORT ------------------------------------------- ***/
 
 export const INSTALL = "curl -fsSL https://disc.sh/install | sh";
-export const VERSION = "2026.06.21";
+export const VERSION = "2026.06.22";
