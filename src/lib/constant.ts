edd799a876750a56908cf2bf3a912021ea937846
netop://ウィビ <paul@webb.page>
2026-09-01 21:53:55 -0700

    version bump

--- src/lib/constant.ts
+++ src/lib/constant.ts
@@ -2,4 +2,4 @@
 /*** EXPORT ------------------------------------------- ***/
 
 export const INSTALL = "curl -fsSL https://disc.sh/install | sh";
-export const VERSION = "2026.06.30";
+export const VERSION = "2026.09.01";
